10309820f6381bc290d4458349460e14ad028cf1
Bill LeBorgne <bill@leborgne.name>
2020-08-07 18:22:19 -0600

    Second exercise after video

--- lib/live_view_studio_web/live/sales_dashboard_live.ex
+++ lib/live_view_studio_web/live/sales_dashboard_live.ex
@@ -4,10 +4,13 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
   alias LiveViewStudio.Sales
 
   def mount(_params, _session, socket) do
+    socket = socket
+      |> assign_stats()
+      |> assign(refresh: 1)
+
     if connected?(socket) do
-      :timer.send_interval(1000, self(), :tick)
+      schedule_refresh(socket)
     end
-    socket = assign_stats(socket)
 
     {:ok, socket}
   end
@@ -42,10 +45,20 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
           </span>
         </div>
       </div>
-      <button phx-click="refresh">
-        <img src="images/refresh.svg">
-        Refresh
-      </button>
+      <div class="controls">
+        <form phx-change="select-refresh">
+          <label for="refresh">
+            Refresh every:
+          </label>
+          <select name="refresh">
+            <%= options_for_select(refresh_options(), @refresh) %>
+          </select>
+        </form>
+        <button phx-click="refresh">
+          <img src="images/refresh.svg">
+          Refresh
+        </button>
+      </div>
     </div>
     """
   end
@@ -55,7 +68,15 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
   end
 
   def handle_info(:tick, socket) do
-    {:noreply, assign_stats(socket)}
+    socket = assign_stats(socket)
+    schedule_refresh(socket)
+    {:noreply, socket}
+  end
+
+  def handle_event("select-refresh", %{"refresh" => refresh}, socket) do
+    refresh = String.to_integer(refresh)
+    socket = assign(socket, refresh: refresh)
+    {:noreply, socket}
   end
 
   defp assign_stats(socket) do
@@ -64,5 +85,13 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
       sales_amount: Sales.sales_amount(),
       satisfaction: Sales.satisfaction()
     )
+  end
+
+  defp refresh_options do
+    [{"1s", 1}, {"5s", 5},{"15s", 15},{"30s", 30},{"60s", 60}]
+  end
+
+  defp schedule_refresh(socket) do
+    Process.send_after(self(), :tick, socket.assigns.refresh * 1000)
   end
 end
